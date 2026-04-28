2374d42a5812ddbfd499865866c5e2e75477e305
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-04-28 09:45:32 +0300

    fix: paths

--- src/main/java/com/example/projecttracker/Controller/ProjectController.java
+++ src/main/java/com/example/projecttracker/Controller/ProjectController.java
@@ -25,7 +25,7 @@ public class ProjectController {
         return new ApiResponse("Project added successfully.");
     }
 
-    @GetMapping("/get/all")
+    @GetMapping("/get-all")
     public ArrayList<Project> getAllProjects(){
         return projects;
     }
@@ -54,7 +54,7 @@ public class ProjectController {
 
 
     //EXTRA ENDPOINTS
-    @PutMapping("/update/status/{id}")
+    @PutMapping("/update-status/{id}")
     public ApiResponse updateStatus(@PathVariable String id){
         for (Project project : projects) {
             if (project.getId().equalsIgnoreCase(id)) {
@@ -65,7 +65,7 @@ public class ProjectController {
         return new ApiResponse("Project with ID: " + id + " not found.");
     }
 
-    @GetMapping("/get/title/{title}")
+    @GetMapping("/get-title/{title}")
     public Project searchByTitle(@PathVariable String title){
         for (Project project : projects) {
             if (project.getTitle().equalsIgnoreCase(title)) {
@@ -75,7 +75,7 @@ public class ProjectController {
         return null;
     }
 
-    @GetMapping("/get/company/{companyName}")
+    @GetMapping("/get-company/{companyName}")
     public ArrayList<Project> getByCompanyName(@PathVariable String companyName){
         ArrayList<Project> companyProjects = new ArrayList<>();
         for(Project project: projects){
